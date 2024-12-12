077ab8e73e15bf45f13be7d2427a30166487830d
Anton Novikov <a.novikov@edenlab.com.ua>
2024-12-12 14:33:21 +0100

    Sort and re-group packages.

--- cmd/main.go
+++ cmd/main.go
@@ -25,20 +25,20 @@ import (
 	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
 	"strings"
 
-	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
-	// to ensure that exec-entrypoint and run can make use of them.
-	_ "k8s.io/client-go/plugin/pkg/client/auth"
-
-	iamv1alpha1 "aws-iam-provisioner.operators.infra/api/v1alpha1"
-	"aws-iam-provisioner.operators.infra/internal/controller"
 	iamctrlv1alpha1 "github.com/aws-controllers-k8s/iam-controller/apis/v1alpha1"
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
+	// to ensure that exec-entrypoint and run can make use of them.
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
 	cpv1beta2 "sigs.k8s.io/cluster-api-provider-aws/v2/controlplane/eks/api/v1beta2"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
+
+	iamv1alpha1 "aws-iam-provisioner.operators.infra/api/v1alpha1"
+	"aws-iam-provisioner.operators.infra/internal/controller"
 	// +kubebuilder:scaffold:imports
 )
 
